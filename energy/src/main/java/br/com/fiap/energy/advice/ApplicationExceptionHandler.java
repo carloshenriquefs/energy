52b76230723784a3aad9cd0afcc653655b8bc5a1
Carlos Henrique <ichfs9108@gmail.com>
2025-05-04 03:29:06 -0300

    feat: configuring stateless authentication

---
+++ energy/src/main/java/br/com/fiap/energy/advice/ApplicationExceptionHandler.java
@@ -0,0 +1,30 @@
+package br.com.fiap.energy.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class ApplicationExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException error) {
+
+        Map<String, String> errorMap = new HashMap<>();
+        List<FieldError> campos = error.getBindingResult().getFieldErrors();
+
+        for (FieldError campo : campos) {
+            errorMap.put(campo.getField(), campo.getDefaultMessage());
+        }
+
+        return errorMap;
+    }
+}
